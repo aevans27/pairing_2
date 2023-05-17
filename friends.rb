4b76f2fab155dc5728ad96503c5a06d411f202a4
Allan Evans <oneblankpage27@gmail.com>
2023-05-16 20:45:37 -0600

    Complete friends

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ arrays = %w[Hanna Joop Allan Cory Cydnee] #Using single word strings
 puts arrays[2]
 puts arrays[-1] #Grabbing last entry of array
 puts arrays[0]
+
+places = ["Tokyo", "Hong Kong", "Kuwait", "Oman", "South Korea"]
+puts places[-2]
